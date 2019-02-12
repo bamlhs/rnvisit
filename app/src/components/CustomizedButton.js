8f34bd69c9c66a53e2a5f7d60976a0a7e535cd69
Abdullah Bamelhes <bamlhs@gmail.com>
2019-02-12 19:05:29 +0300

    add icons and elements

---
+++ app/src/components/CustomizedButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+
+const CustomizedButton = ({
+    params,
+}) => (
+    <View>
+        <Text>CustomizedButton</Text>
+    </View>
+);
+
+export default CustomizedButton;
